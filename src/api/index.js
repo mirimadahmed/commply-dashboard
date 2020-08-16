a7b429afbecf42734b040d79024a4e0ef33414eb
Mir Imad Ahmed <imad.ahmed@gsquad.io>
2020-08-16 13:37:33 +0500

    add owner donre

--- src/api/index.js
+++ src/api/index.js
@@ -53,52 +53,23 @@ export default {
         form.append('company', owner.company_id);
         return axiosObj.post(`/company/owner/update.php`, form)
     },
-    deletOwner(owner) {
+    deleteOwner(owner) {
         let form = new FormData();
         form.append('id', owner.owner_id);
         return axiosObj.post(`/company/owner/delete.php`, form)
     },
+    addOwner(owner) {
+        let form = new FormData();
+        form.append('id', owner.owner_id);
+        form.append('firstname', owner.owner_firstname);
+        form.append('lastname', owner.owner_lastname);
+        form.append('email', owner.owner_email);
+        form.append('password', owner.owner_password);
+        form.append('company', owner.company_id);
+        return axiosObj.post(`/company/owner/add.php`, form)
+    },
     risks() {
         let url = `/company/risks.php`
         return axiosObj.get(url)
-    },
-    users(page) {
-        return axiosObj.get(`/get_users.php?page=${page}`)
-    },
-    activate(id) {
-        let form = new FormData();
-        form.append('id', id);
-        form.append('status', 'active');
-        return axiosObj.post(`/update_userstatus.php`, form)
-    },
-    suspend(id) {
-        let form = new FormData();
-        form.append('id', id);
-        form.append('status', 'suspended');
-        return axiosObj.post(`/update_userstatus.php`, form)
-    },
-    listings(page) {
-        return axiosObj.get(`/get_listings.php?page=${page}&type=tour`)
-    },
-    experiences(page) {
-        return axiosObj.get(`/get_listings.php?page=${page}&type=experience`)
-    },
-    activateListing(id) {
-        let form = new FormData();
-        form.append('id', id);
-        form.append('status', 'active');
-        return axiosObj.post(`/update_listingstatus.php`, form)
-    },
-    suspendListing(id) {
-        let form = new FormData();
-        form.append('id', id);
-        form.append('status', 'suspended');
-        return axiosObj.post(`/update_listingstatus.php`, form)
-    },
-    reportedUsers(page) {
-        return axiosObj.get(`/get_reporteditems.php?page=${page}&type=users`)
-    },
-    reportedListings(page) {
-        return axiosObj.get(`/get_reporteditems.php?page=${page}&type=listings`)
     }
 }
--- src/router/view/owners.vue
+++ src/router/view/owners.vue
@@ -41,7 +41,21 @@ export default {
         { key: "date_created", sortable: true },
         { key: "action", sortable: false },
       ],
-      editedOwner: null
+      editedOwner: null,
+      newOwner: {
+        owner_email: "",
+        owner_password: "",
+        owner_firstname: "",
+        owner_lastname: "",
+        company_id: "",
+      },
+      company_options: [
+        { value: null, text: "Please select an option" },
+        { value: "a", text: "This is First option" },
+        { value: "b", text: "Selected Option" },
+        { value: { C: "3PO" }, text: "This is an option with object value" },
+        { value: "d", text: "This one is disabled", disabled: true },
+      ],
     };
   },
   computed: {
@@ -55,6 +69,7 @@ export default {
   mounted() {
     // Set the initial number of items
     this.fetch();
+    this.fetchCompanies();
   },
   methods: {
     /**
@@ -72,17 +87,34 @@ export default {
       this.totalRows = data.length;
       this.tableData = data;
     },
+    async fetchCompanies() {
+      this.isLoading = true;
+      const { data } = await api.companies();
+      this.isLoading = false;
+      this.company_options = data.map(item => ({value: item.company_id, text: item.company_name}));
+    },
     editOwner(row) {
       this.editedOwner = JSON.parse(JSON.stringify(row.item));
     },
     deleteOwner(row) {
       this.editedOwner = JSON.parse(JSON.stringify(row.item));
     },
+    addNewOwner() {
+      this.newOwner = {
+        owner_email: "",
+        owner_password: "",
+        owner_firstname: "",
+        owner_lastname: "",
+        company_id: "",
+      }
+    },
     async saveOwner() {
       this.isLoading = true;
       const { data } = await api.updateOwner(this.editedOwner);
-      const index = this.tableData.findIndex(item => item.owner_id === this.editedOwner.owner_id);
-      if(index > -1) {
+      const index = this.tableData.findIndex(
+        (item) => item.owner_id === this.editedOwner.owner_id
+      );
+      if (index > -1) {
         this.tableData[index] = JSON.parse(JSON.stringify(this.editedOwner));
       }
       this.isLoading = false;
@@ -90,10 +122,18 @@ export default {
     async saveDeleteOwner() {
       this.isLoading = true;
       const { data } = await api.deleteOwner(this.editedOwner);
-      const index = this.tableData.findIndex(item => item.owner_id === this.editedOwner.owner_id);
-      if(index > -1) {
+      const index = this.tableData.findIndex(
+        (item) => item.owner_id === this.editedOwner.owner_id
+      );
+      if (index > -1) {
         this.tableData.splice(index, 1);
       }
+      this.isLoading = false;
+    },
+    async addOwner() {
+      this.isLoading = true;
+      const { data } = await api.addOwner(this.newOwner);
+      this.tableData.push(data);
       this.isLoading = false;
     }
   },
@@ -111,7 +151,18 @@ export default {
             <b-spinner style="width: 3rem; height: 3rem;" label="Large Spinner"></b-spinner>
           </div>
           <div class="card-body" v-else>
-            <h4 class="card-title">Manage Owners</h4>
+            <div class="row">
+              <div class="col-md-8">
+                <h4 class="card-title">Manage Owners</h4>
+              </div>
+              <div class="col-md-4 text-right">
+                <b-button
+                    v-b-modal.modal-add
+                    @click="addNewOwner"
+                    variant="primary"
+                  >Add Owner</b-button>
+              </div>
+            </div>
             <div class="row mt-4">
               <div class="col-sm-12 col-md-6">
                 <div id="tickets-table_length" class="dataTables_length">
@@ -151,7 +202,7 @@ export default {
                 :filter-included-fields="filterOn"
                 @filtered="onFiltered"
               >
-              <template v-slot:cell(action)="row">
+                <template v-slot:cell(action)="row">
                   <b-button
                     v-b-modal.modal-edit
                     @click="editOwner(row)"
@@ -195,17 +246,17 @@ export default {
             <b-form-group
               id="owner_email"
               label-cols-sm="2"
-              label-cols-lg="2"
+              label-cols-lg="3"
               label="Owner Email"
               label-for="owner_email"
             >
-              <b-form-input id="owner_email" v-model="editedOwner.owner_email" ></b-form-input>
+              <b-form-input id="owner_email" v-model="editedOwner.owner_email"></b-form-input>
             </b-form-group>
-            
+
             <b-form-group
               id="owner_password"
               label-cols-sm="2"
-              label-cols-lg="2"
+              label-cols-lg="3"
               label="Owner Password"
               label-for="owner_password"
             >
@@ -215,7 +266,7 @@ export default {
             <b-form-group
               id="owner_firstname"
               label-cols-sm="2"
-              label-cols-lg="2"
+              label-cols-lg="3"
               label="First Name"
               label-for="owner_firstname"
             >
@@ -225,7 +276,7 @@ export default {
             <b-form-group
               id="owner_lastname"
               label-cols-sm="2"
-              label-cols-lg="2"
+              label-cols-lg="3"
               label="Last Name"
               label-for="owner_lastname"
             >
@@ -247,6 +298,71 @@ export default {
       <div class="row">
         <div class="col-12">
           <p>Are you sure you want to delete this owner?</p>
+        </div>
+      </div>
+    </b-modal>
+    <b-modal
+      id="modal-add"
+      title="Owner"
+      title-class="font-18"
+      ok-title="Create Owner"
+      cancel-title="Cancel"
+      @ok="addOwner"
+    >
+      <h5>Add Owner</h5>
+      <div class="row">
+        <div class="col-12">
+          <form class="form-horizontal" role="form">
+            <b-form-group
+              id="owner_email"
+              label-cols-sm="2"
+              label-cols-lg="3"
+              label="Owner Email"
+              label-for="owner_email"
+            >
+              <b-form-input id="owner_email" v-model="newOwner.owner_email"></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="owner_password"
+              label-cols-sm="2"
+              label-cols-lg="3"
+              label="Owner Password"
+              label-for="owner_password"
+            >
+              <b-form-input for="owner_password" v-model="newOwner.owner_password"></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="owner_firstname"
+              label-cols-sm="2"
+              label-cols-lg="3"
+              label="First Name"
+              label-for="owner_firstname"
+            >
+              <b-form-input for="owner_firstname" v-model="newOwner.owner_firstname"></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="owner_lastname"
+              label-cols-sm="2"
+              label-cols-lg="3"
+              label="Last Name"
+              label-for="owner_lastname"
+            >
+              <b-form-input for="owner_lastname" v-model="newOwner.owner_lastname"></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="owner_company_id"
+              label-cols-sm="2"
+              label-cols-lg="3"
+              label="Company"
+              label-for="owner_company_id"
+            >
+              <b-form-select v-model="newOwner.company_id" :options="company_options"></b-form-select>
+            </b-form-group>
+          </form>
         </div>
       </div>
     </b-modal>
